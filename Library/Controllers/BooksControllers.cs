4bb565fc3ea8abbfc6d55397e66cae4e35389b52
PajocCasa <pajoc527@gmail.com>
2018-12-31 11:55:08 +0000

    com livros

--- Library/Controllers/BooksControllers.cs
+++ Library/Controllers/BooksControllers.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,23 @@ using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
-    [Route("api/books")]
+    [Route("api/author")]
     public class BooksControllers : Controller
     {
-        
+        private ILibraryRepository _libRepository;
+
+        public BooksControllers(ILibraryRepository libRepository)
+        {
+            _libRepository = libRepository;
+        }
+
+        [HttpGet("{author}/books")]
+        public IActionResult GetBooks(Guid author)
+        {
+            var books = _libRepository.GetBooksForAuthor(author);
+
+            return Ok(books);
+
+        }
     }
 }
